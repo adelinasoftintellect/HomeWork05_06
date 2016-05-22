a1e8e150bcc0357919ddcbb0bb24ab7c77157a3b
Adi <adelina_yordanova11@abv.bg>
2016-05-22 16:04:47 +0300

    HomeWork05_06.

--- src/MAin.java
+++ src/MAin.java
@@ -2,16 +2,13 @@
 public class MAin {
 
 	public static void main(String[] args) {
-		int [] a = {4, 5, 7, 8, 9};
+		int[] a = { 4, 5, 7, 8, 9 };
 		printArray(a);
-
 	}
 
 	private static void printArray(int[] b) {
 		for (int i = b.length - 1; i >= 0; i--) {
 			System.out.print(b[i] + " ");
 		}
-		
 	}
-
 }
